1b7b3b35e1785d491c1f3244e076640c6e402cf6
Rafayel Simonyan <rsimonyan@slb.com>
2023-01-20 23:10:09 +0400

    made project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { SinglePhotoComponent } from './components/single-photo/single-photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent,
+    FavoritesComponent,
+    PhotosComponent,
+    SinglePhotoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
